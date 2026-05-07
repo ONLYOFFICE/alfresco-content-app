ef57c644781eed90aec57dd478286e1e76d4cf13
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2026-05-07 09:53:54 +0300

    delete: generated code from projects/onlyoffice-alfresco-extension

--- projects/onlyoffice-alfresco-extension/src/lib/alfresco-extension/alfresco-extension.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-onlyoffice-alfresco-extension',
-  imports: [],
-  templateUrl: './alfresco-extension.component.html',
-  styleUrl: './alfresco-extension.component.css'
-})
-export class OnlyofficeAlfrescoExtensionComponent {}
