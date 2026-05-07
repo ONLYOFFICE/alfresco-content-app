6d737d194ee277c73cf247eada94f65daba5cc7a
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2026-05-07 09:51:31 +0300

    feat: generate @onlyoffice/alfresco-extension

---
+++ projects/onlyoffice-alfresco-extension/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/alfresco-extension/alfresco-extension.component';
---
+++ projects/onlyoffice-alfresco-extension/src/lib/alfresco-extension/alfresco-extension.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-onlyoffice-alfresco-extension',
+  imports: [],
+  templateUrl: './alfresco-extension.component.html',
+  styleUrl: './alfresco-extension.component.css'
+})
+export class OnlyofficeAlfrescoExtensionComponent {}
